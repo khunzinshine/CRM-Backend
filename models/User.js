9a0f44964fac699f257c171bce0f0b283edd77c8
khunzinshine <khunzinshine@ayainnovation.com>
2022-08-28 00:58:38 +0630

    branch out crm app backend

---
+++ models/User.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+UserSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, "Thisismynewtoken");
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
+
+export default mongoose.model("User", UserSchema);
